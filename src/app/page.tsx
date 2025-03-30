809db2a2e662118620618b80f959060be7a6f6ff
Caesar Isidro Va-ay <cnvaay@Caesars-MacBook-Air.local>
2025-03-30 23:20:47 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,91 @@
+import Experience from "@/components/Experience";
+import LinkWithIcon from "@/components/LinkWithIcon";
+import Posts from "@/components/Posts";
+import Projects from "@/components/Projects";
+import Socials from "@/components/Socials";
+import { Button } from "@/components/ui/Button";
+import { getPosts } from "@/lib/posts";
+import {
+  ArrowDownRight,
+  ArrowRightIcon,
+  FileDown
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import path from "path";
+
+const blogDirectory = path.join(process.cwd(), "content");
+const LIMIT = 2; // max show 2
+
+export default async function Home() {
+  const posts = await getPosts(blogDirectory, LIMIT);
+
+  return (
+    <article className="mt-8 flex flex-col gap-16 pb-16">
+      <section className="flex flex-col items-st`art gap-8 md:flex-row-reverse md:items-center md:justify-between">
+        <Image
+          className="rounded-lg"
+          src="/vercel.svg"
+          alt="Photo of Vaay"
+          width={175}
+          height={175}
+          priority
+        />
+        <div className="flex flex-col">
+          <h1 className="title text-5xl">hi vaay here 👋</h1>
+          <p className="mt-4 font-light">
+            {/* Update my age */}
+            21-year-old BSCS student from the Philippines 🇵🇭
+
+
+          </p>
+          <p className="mt-2 font-light">
+          Passionate about agriculture tech, full-stack development, and building smart systems.
+          I fuel my coding sessions with determination (and probably instant coffee). 🚀
+          </p>
+          <div className="mt-4 flex items-end gap-1">
+            <p className="font-semibold">Ask the chatbot anything about me</p>
+            <ArrowDownRight className="size-5 animate-bounce" />
+          </div>
+          <section className="mt-8 flex items-center gap-8">
+            <Link href="/resume.pdf" target="_blank">
+              <Button variant="outline">
+                <span className="font-semibold">Resume</span>
+                <FileDown className="ml-2 size-5" />
+              </Button>
+            </Link>
+            <Socials />
+          </section>
+        </div>
+      </section>
+
+      <Experience />
+
+      <section className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h2 className="title text-2xl sm:text-3xl">featured projects</h2>
+          <LinkWithIcon
+            href="/projects"
+            position="right"
+            icon={<ArrowRightIcon className="size-5" />}
+            text="view more"
+          />
+        </div>
+        <Projects limit={LIMIT} />
+      </section>
+
+      <section className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h2 className="title text-3xl">recent posts</h2>
+          <LinkWithIcon
+            href="/blog"
+            position="right"
+            icon={<ArrowRightIcon className="size-5" />}
+            text="view more"
+          />
+        </div>
+        <Posts posts={posts} />
+      </section>
+    </article>
+  );
+}
